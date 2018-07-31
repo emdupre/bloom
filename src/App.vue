96a829783b0f7cb4ed5eefbfc1b016b8f57b4c55
Elizabeth DuPre <emd222@cornell.edu>
2018-07-31 16:08:52 -0400

    Unmix client side routing, add content

--- src/App.vue
+++ src/App.vue
@@ -8,79 +8,39 @@
           <b-collapse is-nav id="nav_collapse">
             <!--  Here are links to different routes  -->
             <b-navbar-nav class="ml-auto">
-              <b-nav-item to="/about">About</b-nav-item>
+              <!-- <b-nav-item to="/about">About</b-nav-item> -->
               <b-nav-item to="/pubs">Publications</b-nav-item>
               <b-nav-item to="/cv">CV</b-nav-item>
             </b-navbar-nav>
           </b-collapse>
         </b-navbar>
 
-        <!-- Section: intro -->
-        <div class="intro-content">
-            <div class="container">
-                <div class="row">
-
-                    <!--  Headshot image -->
-                    <div class="col-sm-6 col-md-6 col-lg-6">
-                        <img src="../src/assets/me.jpg" class="img-fluid" alt="">
-                    </div>
-
-                    <!-- Landing text, cross-platform links -->
-                    <div class="col-sm-6 col-md-6 col-lg-6 slogan">
-                        <h2>Elizabeth DuPre</h2>
-                        <p>Hi, I'm Elizabeth! I'm a doctoral student in the <a href="//www.mcgill.ca/neuro/neuro-brain-research-patient-care-and-training" target="_blank" style="color: #003399">Montreal Neurological Institute</a> at <a href="//www.mcgill.ca/" target="_blank" style="color: #003399">McGill University</a>.</p>
-                        <p>For more information, please see my <a href="/#/pubs" style="color: #003399">publications</a> and <a href="/#/cv" style="color: #003399">curriculum vitae (CV)</a>.</p>
-
-                        <hr>
-                        <div class="social">
-                            <p>
-                            <a href="//github.com/emdupre" title="Fork me on GitHub" class=github>
-                            <i class="fa fa-github fa-2x"></i></a>
-
-                            <a href="//twitter.com/emdupre_" title="Follow me on Twitter" class=twitter>
-                            <i class="fa fa-twitter fa-2x"></i></a>
-
-                            <a href="//orcid.org/0000-0003-1358-196X" title="View my ORCID profile" class=orcid>
-                            <i class="ai ai-orcid ai-2x"></i></a>
-
-                            <a href="//scholar.google.com/citations?user=l-V-T0sAAAAJ&hl=en" title="View my Google Scholar profile" class=google>
-                            <i class="ai ai-google-scholar ai-2x"></i></a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <router-view></router-view>
         </div>
 
         <footer>
-            <div class="navbar navbar-default bottom"  id="text-center">
-                    <div>
-                    <span class="copy-left"><a href="//copyleft.org/">©</a></span> Elizabeth DuPre 2018
-                    </div>
+            <div class="fluid-container footer">
+                    <p class="text-center"><span class="copy-left"><a href="//copyleft.org/">©</a></span> Elizabeth DuPre 2018</p>
             </div>
         </footer>
-
-        <div class="router"></div>
 
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
-  }
+  name: 'app',
+}
 </script>
 
 <style>
 #app {
   /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-
-#text-center {
-    text-align: center;
 }
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import cv from '@/components/cv'
-import pubs from '@/components/pubs'
+import Home from '@/components/Home'
+// import About from '@/components/About'
+import CV from '@/components/CV'
+import Pubs from '@/components/Pubs'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    // {
+    //   path: '/about',
+    //   name: 'About',
+    //   component: About
+    // },
+    {
       path: '/cv',
-      name: 'cv',
-      component: cv
+      name: 'CV',
+      component: CV
     },
     {
       path: '/pubs',
-      name: 'pubs',
-      component: pubs
+      name: 'Pubs',
+      component: Pubs
     },
   ]
 })
